172d9cc2eb29e06cde352c1105744d35de4151d6
Steven Leclerc <slecler1@stu.norwich.edu>
2015-10-18 17:53:58 -0400

    Initial commit

---
+++ app/src/main/java/cs301/twodoer/Home.java
@@ -0,0 +1,13 @@
+package cs301.twodoer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
