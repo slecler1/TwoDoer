486d0fc3a3c11e3df9ca6080d0c0395fbd88520d
Logan <lmorriso@stu.norwich.edu>
2015-10-25 07:56:07 -0400

    ToDo list updated and working!

--- app/src/main/java/cs301/twodoer/Home.java
+++ app/src/main/java/cs301/twodoer/Home.java
@@ -1,69 +1,116 @@
 package cs301.twodoer;
 
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import cs301.twodoer.TaskContract;
+import cs301.twodoer.TaskDBHelper;
 
 public class Home extends AppCompatActivity {
+    private TaskDBHelper helper;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        // ADD HERE
-        lvItems = (ListView) findViewById(R.id.lvItems);
-        items = new ArrayList<String>();
-        itemsAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, items);
-        lvItems.setAdapter(itemsAdapter);
-        items.add("First Item");
-        items.add("Second Item");
-        // Setup remove listener method call
-        setupListViewListener();
+        updateUI();
+    }
+
+    private void updateUI() {
+        helper = new TaskDBHelper(Home.this);
+        SQLiteDatabase sqlDB = helper.getReadableDatabase();
+        Cursor cursor = sqlDB.query(TaskContract.TABLE,
+                new String[]{TaskContract.Columns._ID, TaskContract.Columns.TASK},
+                null, null, null, null, null);
+
+        SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(
+                this,
+                R.layout.task_view,
+                cursor,
+                new String[]{TaskContract.Columns.TASK},
+                new int[]{R.id.taskTextView},
+                0
+        );
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(listAdapter);
+    }
+
+    public void onDoneButtonClick(View view) {
+        View v = (View) view.getParent();
+        TextView taskTextView = (TextView) v.findViewById(R.id.taskTextView);
+        String task = taskTextView.getText().toString();
+
+        String sql = String.format("DELETE FROM %s WHERE %s = '%s'",
+                TaskContract.TABLE,
+                TaskContract.Columns.TASK,
+                task);
+
+
+        helper = new TaskDBHelper(Home.this);
+        SQLiteDatabase sqlDB = helper.getWritableDatabase();
+        sqlDB.execSQL(sql);
+        updateUI();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu,menu);
         return true;
     }
 
-    private ArrayList<String> items;
-    private ArrayAdapter<String> itemsAdapter;
-    private ListView lvItems;
-
-    public void onAddItem(View v) {
-        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
-        String itemText = etNewItem.getText().toString();
-        itemsAdapter.add(itemText);
-        etNewItem.setText("");
-    }
-
-    // Attaches a long click listener to the listview
-    private void setupListViewListener() {
-        lvItems.setOnItemLongClickListener(
-                new AdapterView.OnItemLongClickListener() {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Add a task");
+                builder.setMessage("What do you want to do?");
+                final EditText inputField = new EditText(this);
+                builder.setView(inputField);
+                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
                     @Override
-                    public boolean onItemLongClick(AdapterView<?> adapter,
-                                                   View item, int pos, long id) {
-                        // Remove the item within array at position
-                        items.remove(pos);
-                        // Refresh the adapter
-                        itemsAdapter.notifyDataSetChanged();
-                        // Return true consumes the long click event (marks it handled)
-                        return true;
-                    }
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String task = inputField.getText().toString();
+                        Log.d("MainActivity",task);
 
+                        TaskDBHelper helper = new TaskDBHelper(Home.this);
+                        SQLiteDatabase db = helper.getWritableDatabase();
+                        ContentValues values = new ContentValues();
+
+                        values.clear();
+                        values.put(TaskContract.Columns.TASK, task);
+
+                        db.insertWithOnConflict(TaskContract.TABLE, null, values,
+                                SQLiteDatabase.CONFLICT_IGNORE);
+                        updateUI();
+                    }
                 });
+
+                builder.setNegativeButton("Cancel",null);
+
+                builder.create().show();
+                return true;
+
+            default:
+                return false;
+        }
     }
 }
-
 
